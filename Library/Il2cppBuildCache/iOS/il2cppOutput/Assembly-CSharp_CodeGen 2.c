cd870cd4fbce6c56635da9f413f2736ad962cb34
sharbel <sharbelhoma@hotmail.com>
2021-12-19 20:58:44 -0600

    updates

---
+++ Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen 2.c
@@ -0,0 +1,55 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void PhoneCamera::Start()
+extern void PhoneCamera_Start_mFA51649FC8B78F6D818D025C13049D6F556B72BC (void);
+// 0x00000002 System.Void PhoneCamera::Update()
+extern void PhoneCamera_Update_mB3B718CEB7AE0213F204931224EB64FA520CC940 (void);
+// 0x00000003 System.Void PhoneCamera::.ctor()
+extern void PhoneCamera__ctor_mC16B617A17351ABE8A2956AF887CBD1C86E9DAF3 (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	PhoneCamera_Start_mFA51649FC8B78F6D818D025C13049D6F556B72BC,
+	PhoneCamera_Update_mB3B718CEB7AE0213F204931224EB64FA520CC940,
+	PhoneCamera__ctor_mC16B617A17351ABE8A2956AF887CBD1C86E9DAF3,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	1120,
+	1120,
+	1120,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
